782ee5f367c99beeca59f5a2363b6ead138a93ab
SSSanga <ksa901125@gmail.com>
2023-05-30 09:29:34 +0900

    백준 1단계

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //백준 10926
+        //입력: 사이트 아이디 중복, 아이디는 소문자만, 길이 < 50
+        //출력: 아이디+??!를 붙여서 나타낼것. 
+        //입력: sanga
+        //출력: sanga??!
+
+    Scanner myObj = new Scanner (System.in);
+    String id = "";
+    id = myObj.nextLine();
+
+    System.out.print (id + "??!");
+    }
+        // return 0 ;
+    }
+
